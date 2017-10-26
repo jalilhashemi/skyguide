fdac7fafcf304b98a91aa25456739adcd886df68
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2017-10-26 15:01:58 +0200

    initialize springMVC project

---
+++ src/main/java/ch/fhnw/skyguide/SkyguideApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.skyguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyguideApplication.class, args);
+	}
+}
