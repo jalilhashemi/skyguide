3132e9e0c1fba543d6dcd9917be028af7f12395d
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2017-11-02 11:19:33 +0100

    again another initialization

--- src/main/java/ch/fhnw/skyguide/SkyguideApplication.java
+++ src/main/java/ch/fhnw/skyguide/SkyguideApplication.java
@@ -2,11 +2,18 @@ package ch.fhnw.skyguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SkyguideApplication {
+public class SkyguideApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkyguideApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SkyguideApplication.class);
 	}
 }
