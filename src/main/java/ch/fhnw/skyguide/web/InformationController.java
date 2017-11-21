9e49b74a790cc5b5d9364388b37a08dc98bfc2eb
Marco Ghilardelli <marco@gawnet.ch>
2017-11-21 13:03:04 +0100

    revert the changes

--- src/main/java/ch/fhnw/skyguide/web/InformationController.java
+++ src/main/java/ch/fhnw/skyguide/web/InformationController.java
@@ -17,7 +17,7 @@ public class InformationController {
     @Autowired
     private ActivityTypeService aircraftTypeService;
 
-    @CrossOrigin//(origins = "http://localhost")
+    @CrossOrigin(origins = "http://localhost")
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public List<ActivityType> getInformations(HttpServletResponse response,
