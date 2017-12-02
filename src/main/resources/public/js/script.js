cd083b6b961a534ab4661afd8a3be6bededfe083
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2017-12-02 11:58:00 +0100

    test cors support on server

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -386,8 +386,10 @@ function initializeMap() {
         // Define the layers to display
         layers: [
             ga.layer.create('ch.swisstopo.pixelkarte-farbe'),
+            ga.layer.create('ch.swisstopo.fixpunkte-agnes'),
             layer
         ],
+        crossOrigin: 'null',
         // Create a view
         //  view: view,
 
