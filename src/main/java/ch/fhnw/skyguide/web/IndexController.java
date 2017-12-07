8b482b9f4909e9ddbcc14acb255e7638752fbbeb
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2017-12-07 10:21:32 +0100

    corrected Controller bug

--- src/main/java/ch/fhnw/skyguide/web/IndexController.java
+++ src/main/java/ch/fhnw/skyguide/web/IndexController.java
@@ -1,9 +1,11 @@
 package ch.fhnw.skyguide.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 @RequestMapping("/")
 @CrossOrigin
 public class IndexController {
