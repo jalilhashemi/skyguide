be8282c50b8a1532517f993ebb8913734c10d31f
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2018-03-22 11:15:17 +0100

    some changes in mail text formatting

--- src/main/java/ch/fhnw/skyguide/util/EmailSender.java
+++ src/main/java/ch/fhnw/skyguide/util/EmailSender.java
@@ -27,61 +27,68 @@ public class EmailSender {
             MimeMessageHelper helper = new MimeMessageHelper(message);
 
             helper.setTo(adminMail);
-            helper.setSubject("There is a new Application");
+            helper.setSubject("There is a new Application (Beta)");
 
             String drawings = "";
             for (Drawing d : application.getDrawings()) {
-                drawings += d.getDrawingType().getName() + ":";
+                drawings += d.getDrawingType().getName() + ": ";
                 for (Coordinate c : d.getCoordinates()) {
-                    drawings += c.getLat() + ", " + c.getLon() + ";";
+                    drawings += "\t" + c.getLat() + ", " + c.getLon() + ";";
                 }
                 drawings += "\n";
             }
 
             String times = "";
             for (Time t : application.getTimes()) {
-                times += t.getStart() + " - " + t.getEnd() + "\n";
+                times += "\t" + t.getStart() + " - " + t.getEnd() + "\n";
             }
 
-            helper.setText("Link: http://localhost:8080?key=" + application.getAdminKey() + "&edit\n\n" +
-                    "Activity Type:\t" + application.getActivityType().getName() + "\n" +
-                    "Aircraft Type:\t" + application.getAircraftType().getName() + "\n" +
-                    "name:\t\t" + application.getName() + "\n" +
-                    "company:\t" + application.getCompany() + "\n" +
-                    "reference:\t" + application.getReference() + "\n" +
-                    "phone:\t" + application.getPhone() + "\n" +
-                    "email:\t\t" + application.getEmail() + "\n" +
-                    "callsign:\t\t" + application.getCallsign() + "\n" +
-                    "departure:\t" + application.getDeparture() + "\n" +
-                    "destination:\t" + application.getDestination() + "\n" +
-                    "date:\t\t" + application.getDateFromUntil() + "\n" +
-                    "duration:\t\t" + application.getDuration() + "\n" +
-                    "location:\t\t" + application.getLocation() + "\n" +
-                    "beam dir:\t" + application.getBeamDirection() + "\n" +
-                    "payload:\t\t" + application.getPayloadAttachedObj() + "\n" +
-                    "amount:\t\t" + application.getAmount() + "\n" +
-                    "radius:\t\t" + application.getRadius() + "\n" +
-                    "self declaration:\t" + application.getSelfDeclaration() + "\n" +
-                    "remark:\t\t" + application.getRemark() + "\n" +
-                    "height:\t\t" + application.getHeightAltitude() + " " + application.getHeightType().getName() + "\n" +
-                    "drawings:\n" + drawings +
-                    "times:\n" + times
+            helper.setText(
+                    "Activity Type:\t" + checkInput(application.getActivityType().getName()) + "\n" +
+                            "Aircraft Type:\t" + checkInput(application.getAircraftType().getName()) + "\n" +
+                            "name:\t\t" + checkInput(application.getName()) + "\n" +
+                            "company:\t" + checkInput(application.getCompany()) + "\n" +
+                            "reference:\t" + checkInput(application.getReference()) + "\n" +
+                            "phone:\t\t" + checkInput(application.getPhone()) + "\n" +
+                            "email:\t\t" + checkInput(application.getEmail()) + "\n" +
+                            "callsign:\t\t" + checkInput(application.getCallsign()) + "\n" +
+                            "departure:\t" + checkInput(application.getDeparture()) + "\n" +
+                            "destination:\t" + checkInput(application.getDestination()) + "\n" +
+                            "date:\t\t" + checkInput(application.getDateFromUntil()) + "\n" +
+                            "duration:\t\t" + checkInput(application.getDuration()) + "\n" +
+                            "location:\t\t" +checkInput( application.getLocation()) + "\n" +
+                            "beam dir:\t\t" + checkInput(application.getBeamDirection()) + "\n" +
+                            "payload:\t\t" +checkInput( application.getPayloadAttachedObj()) + "\n" +
+                            "amount:\t\t" + checkInput(application.getAmount()) + "\n" +
+                            "radius:\t\t" + checkInput(application.getRadius()) + "\n" +
+                            "self decl:\t\t" + checkInput(application.getSelfDeclaration()) + "\n" +
+                            "remark:\t\t" + checkInput(application.getRemark()) + "\n" +
+                            "height:\t\t" + application.getHeightAltitude() + " " + application.getHeightType().getName() + "\n" +
+                            "drawings:\n\t" + drawings +
+                            "times:\n" + times +
+                            "\n\nhttp://localhost:8080?key=" + application.getAdminKey() + "&edit\n"
             );
-        sender.send(message);
+            sender.send(message);
 
-        helper.setTo(application.getEmail());
-        helper.setSubject("Your Application to skyguide");
-        helper.setText("Link: http://localhost:8080?key=" + application.getViewKey());
+            helper.setTo(application.getEmail());
+            helper.setSubject("Your Application to skyguide");
+            helper.setText("Link: http://localhost:8080?key=" + application.getViewKey());
 
-        sender.send(message);
+            sender.send(message);
 
-        return true;
+            return true;
+        } catch (
+                Exception e)
+
+        {
+            return false;
+        }
     }
-        catch(
-    Exception e)
 
-    {
-        return false;
+    public String checkInput(String in) {
+        if (in == "" || in == null)
+            return "-";
+        else
+            return in;
     }
-}
 }
